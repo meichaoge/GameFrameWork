0c91ec9e1dc79b8dedbddca711299e156f80c000
chaoge <386586291@qq.com>
2019-01-07 14:51:40 +0800

    编辑器下清理Console  窗口

---
+++ GameFrameWork/Assets/Editor/EditorConsoleHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameFrameWork.EditorExpand
+{
+    /// <summary>
+    /// 编辑器下Console 面板帮助类
+    /// </summary>
+    public class EditorConsoleHelper : Singleton_Static<EditorHelper>
+    {
+        /// <summary>
+        /// 运行时可以清空Console
+        /// </summary>
+        public static void ClearConsole()
+        {
+            var assembly = Assembly.GetAssembly(typeof(ActiveEditorTracker));
+            var type = assembly.GetType("UnityEditorInternal.LogEntries");
+            if (type == null)
+            {
+                type = assembly.GetType("UnityEditor.LogEntries");
+            }
+            var method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
+        }
+    }
+}
